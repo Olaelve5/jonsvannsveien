0ecb1c7971bd702841aa606ccb5492bf0eda1623
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2023-09-19 23:04:21 +0200

    Navbar component added

--- jonsvannsveien/src/App.tsx
+++ jonsvannsveien/src/App.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
 
 import Home from "./pages/Home";
-import Patricle from "./components/Particle";
 
 import Spline from "@splinetool/react-spline";
 
 function App() {
-
-
   return (
     <>
       {/* <Spline scene="https://prod.spline.design/Ni3jDaKGWW5RrffF/scene.splinecode" /> */}
---
+++ jonsvannsveien/src/components/Navbar.tsx
@@ -0,0 +1,73 @@
+import React, { useState, useRef } from "react";
+
+import "../styles/Navbar.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCloudSun,
+  faCloudMoon,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  const [isMoonClicked, setIsMoonClicked] = useState(false);
+  const [isBarsClicked, setIsBarsClicked] = useState(false);
+
+  const dropDownContainerRef = useRef<HTMLDivElement>(null);
+  const dropDownListRef = useRef<HTMLDivElement>(null);
+
+  const changeColor = () => {
+    if (isMoonClicked) {
+      dropDownContainerRef.current!.style.backgroundColor = "rgb(28, 59, 100)";
+      dropDownListRef.current!.style.color = "white";
+    } else {
+      dropDownContainerRef.current!.style.backgroundColor =
+        "rgb(211, 235, 255)";
+      dropDownListRef.current!.style.color = "black";
+    }
+  };
+
+  const toggleMoon = () => {
+    setIsMoonClicked(!isMoonClicked);
+    changeColor();
+  };
+
+  const toogleDropDown = () => {
+    setIsBarsClicked(!isBarsClicked);
+  };
+
+  return (
+    <div>
+      <div id="navbar-top">
+        <div id="moon-container" onClick={toggleMoon}>
+          {!isMoonClicked ? (
+            <FontAwesomeIcon icon={faCloudMoon} id="navbar-icon" />
+          ) : (
+            <FontAwesomeIcon
+              icon={faCloudSun}
+              flip="horizontal"
+              id="navbar-icon"
+            />
+          )}
+        </div>
+        <div id="menu-icon-container" onClick={toogleDropDown}>
+          <FontAwesomeIcon icon={faBars} id="navbar-icon" />
+        </div>
+      </div>
+      <div
+        id={
+          isBarsClicked ? "drop-down-container-toggled" : "drop-down-container"
+        }
+        ref={dropDownContainerRef}>
+        <div id="drop-down-list" ref={dropDownListRef}>
+          <div className="drop-down-item">Hjem</div>
+          <div className="drop-down-item">Personer</div>
+          <div className="drop-down-item">Øl</div>
+          <div className="drop-down-item">Gjestebok</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
