e0b436c8df11111e90408d9b98d8cd2a5f9dd5fb
Mats Klevstad <matskl@stud.ntnu.no>
2023-10-15 00:10:29 +0200

    connected gjestebok with firebase

--- jonsvannsveien/src/pages/Gjestebok.tsx
+++ jonsvannsveien/src/pages/Gjestebok.tsx
@@ -1,29 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import "../styles/gjestebok.css";
-import { database } from "../App";
+import { firestore } from "../App";
+import { collection, doc, setDoc, getDocs } from "firebase/firestore";
+
+interface Post {
+  id: string;
+  name: string;
+  description: string;
+  date: string;
+}
 
 const Gjestebok = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
   };
 
-  const handleDescriptionChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleDescriptionChange = (event: any) => {
     setDescription(event.target.value);
   };
 
   const handleSubmit = async () => {
-    const postsRef = database.firestore().collection("posts");
+    if (name.trim().length < 1 || description.trim().length < 1)
+      return alert("Du må fylle ut begge feltene");
+    const date = new Date().toLocaleDateString("no-NO");
+    const time = new Date().toLocaleTimeString("no-NO", { hour12: false });
+    const formattedDate = date + " " + time;
 
-    await postsRef.add({
-      name: "formValue",
-      description: "hei",
+    await setDoc(doc(firestore, "posts", formattedDate), {
+      name: name,
+      description: description,
+      date: formattedDate,
     });
+    getPosts();
+    setName("");
+    setDescription("");
+  };
+
+  const getPosts = async () => {
+    const querySnapshot = await getDocs(collection(firestore, "posts"));
+    const postArray: any[] = [];
+
+    querySnapshot.forEach((doc) => {
+      postArray.push(doc.data());
+    });
+    setPosts(postArray);
   };
 
   return (
@@ -31,18 +60,32 @@ const Gjestebok = () => {
       <Navbar />
       <h1>Gjestebok</h1>
       <div className="containerr">
-        <div className="name">
-          Navn: <input value={name} onChange={handleNameChange} type="text" />
-        </div>
-        <div className="description">
-          Beskrivelse:{" "}
-          <input
+        <div className="inputFields">
+          Hvem er du:{" "}
+          <input value={name} onChange={handleNameChange} type="text" />
+          <br />
+          Legg igjen en hilsen:{""}
+          <textarea
+            style={{ height: "100px" }}
             value={description}
             onChange={handleDescriptionChange}
-            type="text"
           />
         </div>
         <button onClick={handleSubmit}>Send</button>
+        <div>
+          {posts
+            .slice()
+            .reverse()
+            .map((post: Post) => (
+              <div key={post.id} className="postContainer">
+                <h1>{post.name}</h1>
+                <p>{post.description}</p>
+                <p>
+                  {post.date}
+                </p>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
